f2be7dec734a3b311cb2fb4bacdd54ca66402074
tolyasjar <toleenjaradat@Toleens-MacBook-Pro.local>
2016-06-22 13:18:49 -0500

    Stepper Control Interface

--- Stepper_Control_Interface/ViewController.m
+++ Stepper_Control_Interface/ViewController.m
@@ -16,12 +16,43 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+
+    UIView *stepperView = [[UIView alloc] init];
+    stepperView.backgroundColor = [UIColor whiteColor];
+    stepperView.frame = CGRectMake(0, 0, 300, 50);
+    
+    stepperView.center = self.view.center;
+    
+    [self.view addSubview:stepperView];
+    
+    
+    UIButton *plusButton = [[UIButton alloc] initWithFrame:CGRectMake(225, 0, 75, 50)];
+    plusButton.backgroundColor = [UIColor lightGrayColor];
+    [plusButton setTitle:@"+" forState: UIControlStateNormal];
+    plusButton.titleLabel.font = [UIFont fontWithName:@"Helvetica" size:25];
+    plusButton.titleLabel.textAlignment = NSTextAlignmentCenter;
+    [stepperView addSubview:plusButton];
+    
+    
+    UIButton *minusButton = [[UIButton alloc] initWithFrame:CGRectMake(0, 0, 75, 50)];
+    minusButton.backgroundColor = [UIColor lightGrayColor];
+    [minusButton setTitle:@"-" forState: UIControlStateNormal];
+    plusButton.titleLabel.font = [UIFont fontWithName:@"Helvetica" size:25];
+    plusButton.titleLabel.textAlignment = NSTextAlignmentCenter;
+    [stepperView addSubview:minusButton];
+    
+    UILabel *stepperLabel = [[UILabel alloc] initWithFrame:CGRectMake(75, 0, 150, 50)];
+    stepperLabel.backgroundColor = [UIColor darkGrayColor];
+    stepperLabel.text = @"5";
+    stepperLabel.font = [UIFont fontWithName:@"Helvetica" size:25];
+    stepperLabel.textAlignment = NSTextAlignmentCenter;
+    [stepperView addSubview:stepperLabel];
+    
+    
 }
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
 }
 
 @end
